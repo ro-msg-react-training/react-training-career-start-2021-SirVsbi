9b8f9ed99faa951144f15d6775a47a7190287f13
vidams <Szabolcs.Vidam@msg.group>
2021-04-28 14:35:17 +0300

    Move product card style to separate file

--- pro-duck-store/src/components/productCard/productCard.tsx
+++ pro-duck-store/src/components/productCard/productCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,18 +7,11 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import ProductModal, {ShowModal} from "./ProductModal";
 import Product from "../../model/Product";
+import {ProductCardStyle} from "../../styles/productCard.style";
 
-const useStyles = makeStyles({
-    root: {
-        maxWidth: 500,
-    },
-    media: {
-        height: 200,
-    },
-});
 
 export default function ProductCard(props: Product) {
-    const classes = useStyles();
+    const classes = ProductCardStyle();
 
     return (
         <Card>
---
+++ pro-duck-store/src/styles/productCard.style.tsx
@@ -0,0 +1,10 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+export const ProductCardStyle = makeStyles({
+    root: {
+        maxWidth: 500,
+    },
+    media: {
+        height: 200,
+    },
+});
